485482b939e16c67a2ac871f988aa63bc10de0ea
czh <czh55@qq.com>
2018-09-27 13:41:23 +0800

    1.改为双通道显示 2.在运行下采样有警告 3.SAC算法时速度太慢，运算不出来

--- ICPDemo/SAC_ICP.cpp
+++ ICPDemo/SAC_ICP.cpp
@@ -1,6 +1,6 @@
 //blog.csdn.net/peach_blossom/article/details/78506184
 
-#define PLY
+#define PCD 
 
 #include <pcl/registration/ia_ransac.h>
 #include <pcl/point_types.h>
@@ -12,6 +12,8 @@
 #include <pcl/filters/filter.h>
 #include <pcl/registration/icp.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/common/transforms.h>  //allows us to use pcl::transformPointCloud function
+
 #include <time.h>
 
 #ifdef PLY
@@ -27,6 +29,100 @@ using pcl::search::KdTree;
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointCloud<PointT> PointCloud;
 
+//icp迭代次数
+int iterations = 50;
+
+void visualization(PointCloud::Ptr source_cloud1_registration,  PointCloud::Ptr cloud_tr, PointCloud::Ptr cloud_icp)
+{
+	// Visualization
+	pcl::visualization::PCLVisualizer viewer("ICP demo");
+	// Create two vertically separated viewports
+	int v1(0);
+	int v2(1);
+	viewer.createViewPort(0.0, 0.0, 0.5, 1.0, v1);
+	viewer.createViewPort(0.5, 0.0, 1.0, 1.0, v2);
+
+	// The color we will be using
+	float bckgr_gray_level = 0.0;  // Black
+	float txt_gray_lvl = 1.0 - bckgr_gray_level;
+
+	// Original point cloud is white
+	pcl::visualization::PointCloudColorHandlerCustom<PointT> cloud_in_color_h(source_cloud1_registration, (int)255 * txt_gray_lvl, (int)255 * txt_gray_lvl,
+		(int)255 * txt_gray_lvl);
+	viewer.addPointCloud(source_cloud1_registration, cloud_in_color_h, "source_cloud1_registration_v1", v1);//白
+	viewer.addPointCloud(source_cloud1_registration, cloud_in_color_h, "source_cloud1_registration_v2", v2);//白
+
+	// Transformed point cloud is green
+	pcl::visualization::PointCloudColorHandlerCustom<PointT> cloud_tr_color_h(cloud_tr, 20, 180, 20);//绿
+	viewer.addPointCloud(cloud_tr, cloud_tr_color_h, "cloud_tr_v1", v1);
+
+	// ICP aligned point cloud is red
+	pcl::visualization::PointCloudColorHandlerCustom<PointT> cloud_icp_color_h(cloud_icp, 180, 20, 20);//红
+	viewer.addPointCloud(cloud_icp, cloud_icp_color_h, "cloud_icp_v2", v2);
+
+	// Orginal point cloud is blue
+	//pcl::visualization::PointCloudColorHandlerCustom<PointT> cloud_all_color_h(cloud_in_all, 0, 191, 255);
+	//viewer.addPointCloud(cloud_in_all, cloud_all_color_h, "cloud_all_v2", v2);
+
+	// Adding text descriptions in each viewport
+	viewer.addText("White: Original point cloud\nGreen: Matrix transformed point cloud", 10, 15, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "icp_info_1", v1);
+	viewer.addText("White: Original point cloud\nRed: ICP aligned point cloud", 10, 15, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "icp_info_2", v2);
+
+	/*std::stringstream ss;
+	ss << iterations;
+	std::string iterations_cnt = "ICP iterations = " + ss.str();
+	viewer.addText(iterations_cnt, 10, 60, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "iterations_cnt", v2);*/
+
+	// Set background color
+	viewer.setBackgroundColor(bckgr_gray_level, bckgr_gray_level, bckgr_gray_level, v1);
+	viewer.setBackgroundColor(bckgr_gray_level, bckgr_gray_level, bckgr_gray_level, v2);
+
+	// Set camera position and orientation
+	viewer.setCameraPosition(-3.68332, 2.94092, 5.71266, 0.289847, 0.921947, -0.256907, 0);
+	viewer.setSize(1280, 1024);  // Visualiser window size
+
+								 // Display the visualiser
+	while (!viewer.wasStopped())
+	{
+		viewer.spinOnce();
+	}
+}
+
+
+void print4x4Matrix(const Eigen::Matrix4d & matrix)
+{
+	printf("Rotation matrix :\n");
+	printf("    | %6.3f %6.3f %6.3f | \n", matrix(0, 0), matrix(0, 1), matrix(0, 2));
+	printf("R = | %6.3f %6.3f %6.3f | \n", matrix(1, 0), matrix(1, 1), matrix(1, 2));
+	printf("    | %6.3f %6.3f %6.3f | \n", matrix(2, 0), matrix(2, 1), matrix(2, 2));
+	printf("Translation vector :\n");
+	printf("t = < %6.3f, %6.3f, %6.3f >\n\n", matrix(0, 3), matrix(1, 3), matrix(2, 3));
+}
+
+
+void transformation(const PointCloud &souce_cloud, PointCloud &cloud_tr) {
+	// Defining a rotation matrix and translation vector
+	Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity();
+
+	// A rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
+	double theta = M_PI / 8;  // The angle of rotation in radians
+	transformation_matrix(0, 0) = cos(theta);
+	transformation_matrix(0, 1) = -sin(theta);
+	transformation_matrix(1, 0) = sin(theta);
+	transformation_matrix(1, 1) = cos(theta);
+
+	// A translation on Z axis (0.4 meters)
+	transformation_matrix(2, 3) = 0.4;
+
+	// Display in terminal the transformation matrix
+	std::cout << "Applying this rigid transformation to: cloud_in -> cloud_icp" << std::endl;
+	print4x4Matrix(transformation_matrix);
+
+	// Executing the transformation
+	pcl::transformPointCloud(souce_cloud, cloud_tr, transformation_matrix);
+
+}
+
 //点云可视化
 void visualize_pcd(PointCloud::Ptr pcd_src,
 	PointCloud::Ptr pcd_tgt,
@@ -40,9 +136,9 @@ void visualize_pcd(PointCloud::Ptr pcd_src,
 	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> src_h(pcd_src, 0, 255, 0);
 	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> tgt_h(pcd_tgt, 255, 0, 0);
 	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> final_h(pcd_final, 0, 0, 255);
-	//viewer.addPointCloud(pcd_src, src_h, "source cloud");
-	viewer.addPointCloud(pcd_tgt, tgt_h, "tgt cloud");
-	viewer.addPointCloud(pcd_final, final_h, "final cloud");
+	viewer.addPointCloud(pcd_src, src_h, "source cloud");//绿
+	viewer.addPointCloud(pcd_tgt, tgt_h, "tgt cloud");//红
+	viewer.addPointCloud(pcd_final, final_h, "final cloud");//蓝
 	//viewer.addCoordinateSystem(1.0);
 	while (!viewer.wasStopped())
 	{
@@ -84,46 +180,48 @@ int
 main(int argc, char** argv)
 {
 	//加载点云文件
-	PointCloud::Ptr cloud_src_o(new PointCloud);//原点云，待配准
-	
+	PointCloud::Ptr cloud_src_o(new PointCloud);//原点云，待配准	
 	PointCloud::Ptr cloud_tgt_o(new PointCloud);//目标点云
-	
+	PointCloud::Ptr cloud_tr_o(new PointCloud);//原点云，待配准,旋转之后	
 
 #ifdef PLY
 	pcl::io::loadPLYFile("bunny_part1.ply", *cloud_src_o);
-	pcl::io::loadPLYFile("bunny_part2.ply", *cloud_tgt_o);
+	pcl::io::loadPLYFile("bunny.ply", *cloud_tgt_o);
 #endif // PLY
 
 #ifdef PCD
-	pcl::io::loadPCDFile("bunny_rotated.pcd", *cloud_src_o);
-	pcl::io::loadPCDFile("bunny.pcd", *cloud_tgt_o);
+	pcl::io::loadPCDFile("rock_part1.pcd", *cloud_src_o);
+	pcl::io::loadPCDFile("rock_part2.pcd", *cloud_tgt_o);
 #endif // PCD
+
+	//点云旋转1/8π
+	transformation(*cloud_src_o, *cloud_tr_o);
 
 	clock_t start = clock();
 	//去除NAN点
 	std::vector<int> indices_src; //保存去除的点的索引
-	pcl::removeNaNFromPointCloud(*cloud_src_o, *cloud_src_o, indices_src);
-	std::cout << "remove *cloud_src_o nan" << endl;
+	pcl::removeNaNFromPointCloud(*cloud_tr_o, *cloud_tr_o, indices_src);
+	std::cout << "remove *cloud_tr_o nan" << endl;
 	//下采样滤波
 	pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
 	voxel_grid.setLeafSize(0.012, 0.012, 0.012);
-	voxel_grid.setInputCloud(cloud_src_o);
-	PointCloud::Ptr cloud_src(new PointCloud);
-	voxel_grid.filter(*cloud_src);
-	std::cout << "down size *cloud_src_o from " << cloud_src_o->size() << "to" << cloud_src->size() << endl;
+	voxel_grid.setInputCloud(cloud_tr_o);
+	PointCloud::Ptr cloud_tr(new PointCloud);
+	voxel_grid.filter(*cloud_tr);
+	std::cout << "down size *cloud_tr_o from " << cloud_tr_o->size() << "to" << cloud_tr->size() << endl;
 	
 
 #ifdef PLY
-	pcl::io::savePLYFileASCII("bunny_src_down.ply", *cloud_src);
+	pcl::io::savePLYFileASCII("bunny_src_down.ply", *cloud_tr);
 #endif // PLY
 
 #ifdef PCD
-	pcl::io::savePCDFileASCII("bunny_src_down.pcd", *cloud_src);
+	pcl::io::savePCDFileASCII("bunny_src_down.pcd", *cloud_tr);
 #endif // PCD
 
 	//计算表面法线
 	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne_src;
-	ne_src.setInputCloud(cloud_src);
+	ne_src.setInputCloud(cloud_tr);
 	pcl::search::KdTree< pcl::PointXYZ>::Ptr tree_src(new pcl::search::KdTree< pcl::PointXYZ>());
 	ne_src.setSearchMethod(tree_src);
 	pcl::PointCloud<pcl::Normal>::Ptr cloud_src_normals(new pcl::PointCloud< pcl::Normal>);
@@ -142,11 +240,11 @@ main(int argc, char** argv)
 	std::cout << "down size *cloud_tgt_o.pcd from " << cloud_tgt_o->size() << "to" << cloud_tgt->size() << endl;
 
 #ifdef PLY
-	pcl::io::savePLYFileASCII("bunny_src_down.ply", *cloud_src);
+	pcl::io::savePLYFileASCII("bunny_src_down.ply", *cloud_tr);
 #endif // PLY
 
 #ifdef PCD
-	pcl::io::savePCDFileASCII("bunny_src_down.pcd", *cloud_src);
+	pcl::io::savePCDFileASCII("bunny_src_down.pcd", *cloud_tr);
 #endif // PCD
 
 	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne_tgt;
@@ -160,14 +258,14 @@ main(int argc, char** argv)
 
 	//计算FPFH
 	pcl::FPFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::FPFHSignature33> fpfh_src;
-	fpfh_src.setInputCloud(cloud_src);
+	fpfh_src.setInputCloud(cloud_tr);
 	fpfh_src.setInputNormals(cloud_src_normals);
 	pcl::search::KdTree<PointT>::Ptr tree_src_fpfh(new pcl::search::KdTree<PointT>);
 	fpfh_src.setSearchMethod(tree_src_fpfh);
 	pcl::PointCloud<pcl::FPFHSignature33>::Ptr fpfhs_src(new pcl::PointCloud<pcl::FPFHSignature33>());
 	fpfh_src.setRadiusSearch(0.05);
 	fpfh_src.compute(*fpfhs_src);
-	std::cout << "compute *cloud_src fpfh" << endl;
+	std::cout << "compute *cloud_tr fpfh" << endl;
 
 	pcl::FPFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::FPFHSignature33> fpfh_tgt;
 	fpfh_tgt.setInputCloud(cloud_tgt);
@@ -181,7 +279,7 @@ main(int argc, char** argv)
 
 	//SAC配准
 	pcl::SampleConsensusInitialAlignment<pcl::PointXYZ, pcl::PointXYZ, pcl::FPFHSignature33> scia;
-	scia.setInputSource(cloud_src);
+	scia.setInputSource(cloud_tr);
 	scia.setInputTarget(cloud_tgt);
 	scia.setSourceFeatures(fpfhs_src);
 	scia.setTargetFeatures(fpfhs_tgt);
@@ -195,28 +293,33 @@ main(int argc, char** argv)
 	sac_trans = scia.getFinalTransformation();
 	std::cout << sac_trans << endl;
 #ifdef PLY
-	pcl::io::savePLYFileASCII("bunny_src_down.ply", *cloud_src);
+	pcl::io::savePLYFileASCII("bunny_src_down.ply", *cloud_tr);
 #endif // PLY
 
 #ifdef PCD
-	pcl::io::savePCDFileASCII("bunny_src_down.pcd", *cloud_src);
+	pcl::io::savePCDFileASCII("bunny_src_down.pcd", *cloud_tr);
 #endif // PCD
 	clock_t sac_time = clock();
+	std::cout << "SAC done" << endl;
+	//显示SAC效果
+	visualization(cloud_tgt, cloud_tr, sac_result);
 
+	/*
 	//icp配准
 	PointCloud::Ptr icp_result(new PointCloud);
 	pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-	icp.setInputSource(cloud_src);
+	icp.setInputSource(cloud_tr);
 	icp.setInputTarget(cloud_tgt_o);
 	//Set the max correspondence distance to 4cm (e.g., correspondences with higher distances will be ignored)
 	icp.setMaxCorrespondenceDistance(0.04);
 	// 最大迭代次数
-	icp.setMaximumIterations(50);
+	icp.setMaximumIterations(iterations);
 	// 两次变化矩阵之间的差值
 	icp.setTransformationEpsilon(1e-10);
 	// 均方误差
 	icp.setEuclideanFitnessEpsilon(0.2);
 	icp.align(*icp_result, sac_trans);
+	//icp.align(*icp_result);
 
 	clock_t end = clock();
 	cout << "total time: " << (double)(end - start) / (double)CLOCKS_PER_SEC << " s" << endl;
@@ -231,15 +334,19 @@ main(int argc, char** argv)
 	//cout<<"ransformationProbability"<<icp.getTransformationProbability()<<endl;
 	std::cout << icp_trans << endl;
 	//使用创建的变换对未过滤的输入点云进行变换
-	pcl::transformPointCloud(*cloud_src_o, *icp_result, icp_trans);
+	pcl::transformPointCloud(*cloud_tr_o, *icp_result, icp_trans);
 	//保存转换的输入点云
 #ifdef PLY
-	pcl::io::savePLYFileASCII("bunny_src_down.ply", *cloud_src);
+	pcl::io::savePLYFileASCII("bunny_src_down.ply", *cloud_tr);
 #endif // PLY
 
 #ifdef PCD
-	pcl::io::savePCDFileASCII("bunny_src_down.pcd", *cloud_src);
+	pcl::io::savePCDFileASCII("bunny_src_down.pcd", *cloud_tr);
 #endif // PCD
+
+	std::cout << "ICP done" << endl;
+	//显示ICP效果
+	visualization(cloud_tgt_o, cloud_tr, icp_result);
 
 	//计算误差
 	Eigen::Vector3f ANGLE_origin;
@@ -254,6 +361,7 @@ main(int argc, char** argv)
 	cout << "error in aixs_x: " << error_x << "  error in aixs_y: " << error_y << "  error in aixs_z: " << error_z << endl;
 
 	//可视化
-	visualize_pcd(cloud_src_o, cloud_tgt_o, icp_result);
+	visualize_pcd(cloud_tr_o, cloud_tgt_o, icp_result);
+	*/
 	return (0);
 }
