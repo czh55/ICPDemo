b335d97df2422d48781e6e913fd6a46df81b1039
czh <czh55@qq.com>
2018-10-10 12:41:20 +0800

    在PCD2PLY.cpp添加PCD2PLY的转换功能，最后是可以产生.ply文件的，但是在cloudcompare中打开的时候，根据转换的数据不同，会有不同的问题

---
+++ ICPDemo/PCD2PLY.cpp
@@ -0,0 +1,29 @@
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/PCLPointCloud2.h>
+#include <iostream>
+#include <string>
+using namespace pcl;
+using namespace pcl::io;
+using namespace std;
+int PCDtoPLYconvertor(string & input_filename, string& output_filename)
+{
+	pcl::PCLPointCloud2 cloud;
+	if (loadPCDFile(input_filename, cloud) < 0)
+	{
+		cout << "Error: cannot load the PCD file!!!" << endl;
+		return -1;
+	}
+	PLYWriter writer;
+	writer.write(output_filename, cloud, Eigen::Vector4f::Zero(), Eigen::Quaternionf::Identity(), true, true);
+	return 0;
+}
+int main()
+{
+	string input_filename = "data/rock_all.pcd";
+	string output_filename = "result/rock_all.ply";
+	PCDtoPLYconvertor(input_filename, output_filename);
+	system("pause");
+	return 0;
+}
+
